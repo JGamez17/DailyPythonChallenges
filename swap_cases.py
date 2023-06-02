258f68b5c1842fa4320e4b9c4b31da8d006689c1
JGamez17 <github email address>
2023-06-02 09:11:42 -0700

    completed challenege - swapCase

---
+++ swap_cases.py
@@ -0,0 +1,12 @@
+# make lower case letters uppercase and vise versa
+# HackerRank.com presents "Pythonist 2". (input)
+def swap_case(s):
+
+    return s.swapcase()
+    # swapcase is a method
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
